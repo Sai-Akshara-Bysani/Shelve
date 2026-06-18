5371d58a6a1b7d7923f79b3188262cb29bfaac8a
Akshara <aksharabysani2@gmail.com>
2026-06-18 15:37:15 +0530

    deploy anf stuff

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,24 @@
+# import mysql.connector
+
+# def get_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="Akki$123",
+#         database="shelve"
+#     )
+
+import os
+from dotenv import load_dotenv
 import mysql.connector
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Akki$123",
-        database="shelve"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
