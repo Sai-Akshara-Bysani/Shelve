49671aa503e6826b04414844aacdbd9ad2d0fbaa
Akshara <aksharabysani2@gmail.com>
2026-06-18 17:34:44 +0530

    fix

--- backend/main.py
+++ backend/main.py
@@ -9,11 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://shelve-qjkx.onrender.com"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
